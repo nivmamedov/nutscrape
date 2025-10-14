3e15ab5d32eaa26b781a3267b61e87dfe5eae171
Niv Mamedov <nivmamedov@gmail.com>
2025-10-14 11:44:21 +0300

    add swagger to nutscrapi

--- apps/nutscrapi/src/main.ts
+++ apps/nutscrapi/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nutscrapi')
+    .setDescription('An API that will get you the secret sauce you need.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 80);
 }
 bootstrap();
